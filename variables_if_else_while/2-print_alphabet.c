ec731ccf1f65bd93e6cc8800fce1b31a75c19f73
Your Name <you@example.com>
2022-10-06 17:31:41 -0700

    nuevo

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int c = 97;
