a8391a009ab36da4d4a4c1a63cc350aeeda5ce41
Your Name <you@example.com>
2022-10-06 18:04:19 -0700

    nuevo

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
