0c55a6d464d4bb9d1d28c0e39b449523e08d735a
Your Name <you@example.com>
2022-10-06 18:49:32 -0700

    nuevo

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
