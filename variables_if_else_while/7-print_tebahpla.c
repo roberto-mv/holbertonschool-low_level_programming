da1a7b3daa25a70264d8a49daa8b0de58a0b0dbb
Your Name <you@example.com>
2022-10-06 18:54:28 -0700

    nuevo

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Letter
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
