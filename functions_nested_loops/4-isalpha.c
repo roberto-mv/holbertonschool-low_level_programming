b2e14617c506bc3d7e7da401d14c824e3aed386b
Your Name <you@example.com>
2022-10-07 22:00:30 -0700

    nuevo

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,7 +3,9 @@
 /**
  * _isalpha - returns whether c is an alpha character
  *
- * @c: ascii value of a charactetter, 0 otherwise
+ * @c: ascii value of a character we're determining if is alpha
+ *
+ * Return: 1 is c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
