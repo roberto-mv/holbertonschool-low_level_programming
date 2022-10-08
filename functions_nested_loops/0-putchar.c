d8d7bc3f2eed6e1d1fa210a182306e5be35f1502
Your Name <you@example.com>
2022-10-07 17:30:00 -0700

    nuevo

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - putchar
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+	printf("_putchar\n");
+	return (0);
 }
