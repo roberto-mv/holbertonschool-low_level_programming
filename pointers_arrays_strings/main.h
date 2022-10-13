8f52eecc3bf977c0a46b99b35de0cea2318d842d
Your Name <you@example.com>
2022-10-12 17:47:12 -0700

    nuevo

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,4 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
