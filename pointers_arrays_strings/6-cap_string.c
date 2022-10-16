fe747f2d583f69ec29916a06814e825ad983267e
Your Name <you@example.com>
2022-10-16 10:17:10 -0700

    more pointers

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - a function
+ *
+ * @s: a pointer
+ *
+ * Return: return a string.
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+	while (s[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
