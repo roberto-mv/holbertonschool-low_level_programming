aaefd09d238dac6e46edc856d7cf8d63de44699f
roberto-mv <113920469+roberto-mv@users.noreply.github.com>
2022-11-18 10:12:52 -0500

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,19 +9,21 @@
  * Return: return 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int res1, res2;
+	int n1, n2;
 
-	res1 = res2 = 0;
 	if (argc == 3)
 	{
-		res1 = atoi(argv[1]);
-		res2 = atoi(argv[2]);
-		printf("%d\n", res1 * res2);
-		return (0);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
 	}
 	else
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
+
+	return (0);
 }
