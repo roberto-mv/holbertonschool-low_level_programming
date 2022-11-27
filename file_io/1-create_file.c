e750cfe1c6cc9d3b247bced104b12922248940d0
roberto-mv <5813@holbertonstudents.com>
2022-11-27 12:09:01 -0800

    File I/O

---
+++ file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name the file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: return
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, res;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	for (i = 0; text_content[i] != 0; i++)
+		;
+
+	res = write(fd, text_content, i);
+	close(fd);
+
+	return (res == -1 ? -1 : 1);
+}
