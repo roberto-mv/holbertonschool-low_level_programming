375088c516b08482414bb4520fbb0c57f3152bea
roberto-mv <5813@holbertonstudents.com>
2022-11-27 10:34:57 -0800

    Singly linked lists

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - Adds a new node 
+ * @head: Head
+ * @str: New value
+ *
+ * Return: Return the new node
+ **/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int len;
+	list_t *new;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != 0; len++)
+		;
+
+	new->str = malloc(len + 1);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	for (; *head; )
+		head = &(*head)->next;
+
+	*head = new;
+
+	return (new);
+}
