c0958c8d49babe218a03da0a88dd4005b47b1864
roberto-mv <5813@holbertonstudents.com>
2022-11-27 10:35:52 -0800

    Singly linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints elements of the list
+ * @h: Head of the list
+ * Return: Numbers
+ **/
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; h; i++)
+	{
+		printf("[%u] %s\n", (h->str != NULL) ? h->len : 0,
+				(h->str != NULL) ? h->str : "(nil)");
+		h = h->next;
+	}
+
+	return (i);
+}
