848ae268e73c79a586d911f30a6a21c1753577be
roberto-mv <5813@holbertonstudents.com>
2022-11-27 16:05:31 -0800

    Hash tables

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints hash table
+ * @ht: Hash table
+ **/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int idx, end;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (idx = 0, end = 1; idx < ht->size; idx++)
+	{
+		for (tmp = ht->array[idx]; tmp != NULL; tmp = tmp->next)
+		{
+			printf("%s'%s': '%s'", end == 1 ? "" : ", ", tmp->key, tmp->value);
+			end = 0;
+		}
+	}
+	printf("}\n");
+
+}
