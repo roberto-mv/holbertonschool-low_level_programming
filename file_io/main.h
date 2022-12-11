f79533e2ca2c21a4750a0d0306abdad2611d949b
roberto-mv <5813@holbertonstudents.com>
2022-12-11 15:10:35 -0800

    nuevo

--- file_io/main.h
+++ file_io/main.h
@@ -3,12 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#define BUFFSIZE 1024
+
 
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _err(int stat, ...);
 
 #endif
