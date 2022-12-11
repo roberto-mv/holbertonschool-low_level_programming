daaab1471ead8e453246427886f91f1372ccf547
roberto-mv <5813@holbertonstudents.com>
2022-12-11 13:16:12 -0800

    nuevo

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -2,21 +2,22 @@
 /**
  * print_number - prints an integer
  * @n:.input integer parameter
- *
- *
  */
+
 void print_number(int n)
 {
-	unsigned int i = n;
-
-	if (n < 0)
+	if (n == 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('0');
+		return;
 	}
-	if (i / 10)
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	if ((n / 10) != 0)
 	{
-		print_number(i / 10);
+		print_number((n / 10) * -1);
 	}
-	_putchar(i % 10 + '0');
+	_putchar((n % 10) * -1 + '0');
 }
