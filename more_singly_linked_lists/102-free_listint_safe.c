2d9e01802f636b74f79c8b2a430d1566ff6522bd
roberto-mv <5813@holbertonstudents.com>
2022-12-11 14:37:03 -0800

    nuevo

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,41 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - function to free list
- * @h: pointer to the pointer of the list
- * Return: count
+ * free_listint_safe - frees a list (safe version)
+ * @h: linked list of type listint_t
+ *
+ * Return: number of nodes in freed list
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count_new = 0, count_comp = 0;
-	listint_t *tmp, *head, *comp;
+	listint_t *current, *runner, *head;
+	size_t h_i, r_i;
 
 	if (h == NULL || *h == NULL)
 		return (0);
-	head = comp = tmp = *h;
-	count_new = 0;
+
+	current = *h;
+	head = *h;
+	h_i = 0;
+
 	while (head != NULL)
 	{
-		comp = *h;
-		count_comp = 0;
-		while (count_new > count_comp)
+		runner = *h;
+		for (r_i = 0; r_i < h_i; r_i++)
 		{
-			if (tmp == comp)
+			if (runner == current)
 			{
 				*h = NULL;
-				return (count_new);
+				return (h_i);
 			}
-			count_comp++;
-			comp = comp->next;
+			runner = runner->next;
 		}
-		count_new++;
-		tmp = head->next;
-		free((void *)head);
-		head = tmp;
+		current = head->next;
+		free(head);
+		head = current;
+		h_i++;
 	}
-	*h = tmp;
-	return (count_new);
+	*h = NULL;
+	return (h_i);
 }
